f367b977426572e493de85acbd6c9e73cc8c9518
victormanuel-araujo <victormanuel.araujo@outlook.com>
2021-06-23 19:17:55 -0300

    feat: compared tabulation with memoization on fibonacci

--- data-structures/fibonacci.js
+++ data-structures/fibonacci.js
@@ -1,10 +1,23 @@
-function fib(n) {
-	if (fib.prototype.cache[n]) return fib.prototype.cache[n];
+// Memoization
+function fibMemo(n) {
+	if (fibMemo.prototype.cache[n]) return fibMemo.prototype.cache[n];
 	if (n <= 2) return 1;
 
-	const returnValue = fib(n - 1) + fib(n - 2);
-	fib.prototype.cache[n] = returnValue;
+	const returnValue = fibMemo(n - 1) + fibMemo(n - 2);
+	fibMemo.prototype.cache[n] = returnValue;
 	return returnValue;
 }
 
-fib.prototype.cache = {};
+fibMemo.prototype.cache = {};
+
+// Tabulation - Better space complexity
+function fiboTabulation(n) {
+	if (n <= 2) return 1;
+	const fibNums = [0, 1, 1];
+
+	for (let i = 3; i <= n; i++) {
+		fibNums[i] = fibNums[i - 1] + fibNums[i - 2];
+	}
+
+	return fibNums[n];
+}
